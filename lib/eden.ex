64781e296b2b15013ff6191f01934fa6b70762a4
Amy Null <amy@willnot.date>
2017-10-24 08:37:45 -0400

    Just use :init.stop/0

--- lib/eden.ex
+++ lib/eden.ex
@@ -55,7 +55,7 @@ defmodule Eden do
       registry_dir: "eden_registry_" <> to_string(state[:name])
     }
     # Handle POSIX signals
-    System.SignalHandler.register :term, fn -> Eden.terminate(":term trap", state) end
+    System.SignalHandler.register :term, &:init.stop/0
     send self(), :connect
 
     {:ok, state}
